6507dcfede50bcd0a3b6f62c47c6292aeb04e36c
michael morales <maicolysofia55@gmail.com>
2023-11-13 15:17:03 -0500

    actualizado

--- archi.js/js.js
+++ archi.js/js.js
@@ -1,2 +1,16 @@
-let = "";
-let ="";
+function login() {
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    if (username === "mai" && password === "123") {
+        alert("Inicio de sesión exitoso. ¡Bienvenido, " + username + "!");
+        window.location.href = "http://127.0.0.1:5500/pagi.html";
+    } else {
+        alert("Nombre de usuario o contraseña incorrectos. ");
+    }
+}
+
+
+
+
+
